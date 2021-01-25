d48e62100e4538bdcafbf31f6bf2c0e3111fa055
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-01-25 14:30:43 -0500

    Initial scaffolding works

---
+++ src/webparts/gridcharts/components/Gridcharts.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import styles from './Gridcharts.module.scss';
+import { IGridchartsProps } from './IGridchartsProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+/**
+ * Based upon example from
+ * https://codepen.io/ire/pen/Legmwo
+ */
+
+
+export default class Gridcharts extends React.Component<IGridchartsProps, {}> {
+  public render(): React.ReactElement<IGridchartsProps> {
+    return (
+      <div className={ styles.gridcharts }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={ styles.button }>
+                <span className={ styles.label }>Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/webparts/gridcharts/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGridchartsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GridchartsWebPartStrings' {
+  const strings: IGridchartsWebPartStrings;
+  export = strings;
+}
