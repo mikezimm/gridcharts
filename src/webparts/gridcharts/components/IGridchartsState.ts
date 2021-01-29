6a8d0abd2513813e2657d5f0835b1deab91872ce
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-01-28 23:35:19 -0500

    Add state props for future filters

--- src/webparts/gridcharts/components/IGridchartsState.ts
+++ src/webparts/gridcharts/components/IGridchartsState.ts
@@ -7,10 +7,16 @@ export interface IGridchartsData {
 
 export interface IGridchartsState {
 
-    gridData: IGridchartsData[];
-    monthLables: string[];
-    monthScales: number[];
-    entireDateArray: any[];
+    selectedYear: number; //Used to determine selected Year Pivot
+    selectedUser: any; //Used to determine filter of items ( current user or everyone )
+
+    gridData: IGridchartsData[]; //One IGridchartsData per date between lowest and highest date range for input data
+
+    monthLables: string[];  //Used to build the month labels on top of the gridChart
+    monthScales: number[];  //Used to space the month labels on top of the gridChart
+
+    entireDateArray: any[];  //Used as easy date index of entire range of data... to easily find correct item in gridData
+
     WebpartHeight?:  number;    //Size courtesy of https://www.netwoven.com/2018/11/13/resizing-of-spfx-react-web-parts-in-different-scenarios/
     WebpartWidth?:   number;    //Size courtesy of https://www.netwoven.com/2018/11/13/resizing-of-spfx-react-web-parts-in-different-scenarios/
 
